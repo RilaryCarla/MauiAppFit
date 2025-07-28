b5dda1079547dec63a15d77fec34cbae7866f3a9
231145 <231145@CPD03-M17>
2025-07-28 15:54:14 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppFit/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.Contracts;
+using MauiAppFit.Helpers;
+
+namespace MauiAppFit
+{
+    public partial class App : Application
+    {
+        static SQLiteDataBaseHelper database;
+
+        public static SQLiteDataBaseHelper Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new SQLiteDataBaseHelper(
+                        Path.Combine(Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                            "XamAppFit.db3"));
+                }
+
+                return database;
+            }
+        }
+    }
+}
